91245f3f75f07345a0c7b895fb664ea4a2d1290d
michaelsong5713 <kvgfcmxmqx@privaterelay.appleid.com>
2026-06-04 22:00:49 -0400

    bugs

--- main.py
+++ main.py
@@ -12,10 +12,17 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 if torch.cuda.is_available():
     print("GPU Name:", torch.cuda.get_device_name(0),"\n")
 
+def t_transforms_fn(image, mask):
+    return train_transform(image), mask_transform(mask)
+
+def v_transforms_fn(image, mask):
+    return train_transform(image), mask_transform(mask)
+
 train_data = datasets.OxfordIIITPet(
     root = "data",
     split = "trainval",
     target_types = "segmentation",
+    transforms = t_transforms_fn,
     download = True
 )
 
@@ -23,6 +30,7 @@ test_dataset = datasets.OxfordIIITPet(
     root = "data",
     split = "test",
     target_types = "segmentation",
+    transforms = v_transforms_fn,
     download = True
 )
 
@@ -65,8 +73,18 @@ test_transform = v2.Compose([
     v2.Normalize(mean=mean,std=std)
 ])
 
+mask_transform = v2.Compose([
+    v2.Resize((256, 256), interpolation=v2.InterpolationMode.NEAREST),
+    v2.ToImage(),
+    v2.Lambda(lambda x: x.squeeze(0).long() - 1)
+])
+
+'''
 train_data.transform = train_transform
 test_dataset.transform = test_transform
+train_data.target_transform = mask_transform
+test_dataset.target_transform = mask_transform
+'''
 
 training_len = len(train_data)
 val_size = int(training_len*0.2)
@@ -78,7 +96,7 @@ train_dataset,val_dataset = random_split(
 
 train_dataloader = DataLoader(train_dataset,batch_size=64,shuffle=True)
 val_dataloader = DataLoader(val_dataset,batch_size=64,shuffle=False)
-train_dataloader = DataLoader(test_dataset,batch_size=64,shuffle=False)
+test_dataloader = DataLoader(test_dataset,batch_size=64,shuffle=False)
 
 class double_conv_block(nn.Module):
     def __init__(self,in_ch,out_ch):
@@ -92,20 +110,100 @@ class double_conv_block(nn.Module):
             nn.ReLU()
         )
     def forward(self,x):
+        return self.block(x)   
+
+class down(nn.Module):
+    def __init__(self,in_ch,out_ch):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            double_conv_block(in_ch,out_ch)
+        )
+    def forward(self,x):
         return self.block(x)
 
-
-temp = double_conv_block(3,64)
-test = torch.randn(1,3,256,256)
-out = temp(test)
-print(out.shape)
+class up(nn.Module):
+    def __init__(self,in_ch,out_ch):
+        super().__init__()
+        self.upscale = nn.ConvTranspose2d(in_ch,out_ch,kernel_size=2,stride=2)
+        self.conv = double_conv_block(in_ch,out_ch)
+    def forward(self,input,skip):
+        input = self.upscale(input)
+        return self.conv(torch.cat([input,skip],dim=1))
 
 class neural_network(nn.Module):
-    def __init__(self):
+    def __init__(self, in_ch = 3, features = 64, classes = 3):
         super().__init__()
+        #start at 256x256
+        self.encoder1 = double_conv_block(in_ch, features) #256x256 64 channels
+        self.encoder2 = down(features, features*2) #128x128 128 channels
+        self.encoder3 = down(features*2, features*4) #64x64 256 channels
+        self.encoder4 = down(features*4, features*8) #32x32 512 channels
+
+        self.bottleneck = down(features*8,features*16) #16x16 1024 channels
+
+        self.decoder4 = up(features*16,features*8)
+        self.decoder3 = up(features*8,features*4)
+        self.decoder2 = up(features*4,features*2)
+        self.decoder1 = up(features*2,features)
+
+        self.output = nn.Conv2d(features,classes,kernel_size=1)
+        #encoder
+
     def forward(self,x):
-        pass
-    def train(self):
-        pass
+        e1 = self.encoder1(x)
+        e2 = self.encoder2(e1)
+        e3 = self.encoder3(e2)
+        e4 = self.encoder4(e3)
+
+        b1 = self.bottleneck(e4)
+
+        d = self.decoder4(b1,e4)
+        d = self.decoder3(d,e3)
+        d = self.decoder2(d,e2)
+        d = self.decoder1(d,e1)
+
+        return self.output(d)
+    def train_epoch(self,t_dataloader,v_dataloader,loss_fn,optimizer,scheduler,device):
+        self.train()
+        train_loss = 0
+        num = 0
+        for batch_d,batch_l in t_dataloader:
+            batch_d, batch_l = batch_d.to(device), batch_l.to(device)
+            #print(f"Mask Shape: {batch_l.shape}")
+            output = self(batch_d)
+            loss = loss_fn(output,batch_l)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+            train_loss+=loss.item()
+            num+=1
+        train_avg_loss = train_loss/num
+        print(f"Training Loss: {train_avg_loss}")
+        self.eval()
+        val_loss = 0
+        num = 0
+        with torch.no_grad():
+            for batch_d,batch_l in v_dataloader:
+                batch_d, batch_l = batch_d.to(device), batch_l.to(device)
+                output = self(batch_d)
+                loss = loss_fn(output,batch_l)
+                val_loss+=loss.item()
+                num+=1
+        val_avg_loss = val_loss/num
+        print(f"Validation Loss: {val_avg_loss}")
+        scheduler.step()
     def test(self):
+
         pass
+
+generations=10
+model = neural_network().to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.001)
+scheduler = CosineAnnealingLR(optimizer,T_max=generations)
+
+for i in range(generations):
+    print(f"Epoch: {i+1}\n")
+    model.train_epoch(train_dataloader,val_dataloader,loss_fn,optimizer,scheduler,device)
+
